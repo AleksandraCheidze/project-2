91f2299fd365a27ad0b32682da0d9a71eb7f315c
Aleksandra Cheidze <a.cxeidze@gmail.com>
2023-08-11 19:30:08 +0200

    feat: replace to Integer.compare

--- src/PriorityComparator.java
+++ src/PriorityComparator.java
@@ -6,22 +6,14 @@ public class PriorityComparator implements Comparator<Task> {
     int priority1 = task1.getPriority();
     int priority2 = task2.getPriority();
 
-    // Выполненные задачи всегда идут в конце списка
+
     if (task1.isCompleted() && !task2.isCompleted()) {
       return 1;
     }
     if (!task1.isCompleted() && task2.isCompleted()) {
       return -1;
     }
-
-    // Сортировка по приоритету
-    if (priority1 < priority2) {
-      return -1;
-    } else if (priority1 > priority2) {
-      return 1;
-    } else {
-      return 0;
-    }
+    return Integer.compare(priority1, priority2);
   }
 }
 
