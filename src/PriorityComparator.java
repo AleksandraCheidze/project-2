305d896d351b885e45ffb47f059d896f3ee02946
Aleksandra Cheidze <a.cxeidze@gmail.com>
2023-08-13 11:53:34 +0200

    feat: add reverso

--- src/PriorityComparator.java
+++ src/PriorityComparator.java
@@ -11,17 +11,16 @@ public class PriorityComparator implements Comparator<Task> {
     if (dateComparison != 0) {
       return dateComparison;
     }
+
     int priority1 = task1.getPriority();
     int priority2 = task2.getPriority();
 
-
-    if (task1.isCompleted() && !task2.isCompleted()) {
+    if (task2.isCompleted() && !task1.isCompleted()) {
       return 1;
     }
-    if (!task1.isCompleted() && task2.isCompleted()) {
+    if (!task2.isCompleted() && task1.isCompleted()) {
       return -1;
     }
-    return Integer.compare(priority1, priority2);
+    return Integer.compare(priority2, priority1);
   }
 }
-
