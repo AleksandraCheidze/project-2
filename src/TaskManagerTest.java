4f5f8433232ab58acb215e4348339d4214decada
Dmitrijs Loginovs <loginovs.dmitrijs@gmail.com>
2023-08-13 17:20:25 +0200

    Feat: add testGetUserChoiceValidInput

--- src/TaskManagerTest.java
+++ src/TaskManagerTest.java
@@ -1,4 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +12,7 @@ public class TaskManagerTest {
     TaskManager taskManager = new TaskManager();
     Task task1 = new Task("Task 1");
     Task task2 = new Task("Task 2");
-    taskManager.getTasks().add(task1);
+    taskManager().add(task1);
     taskManager.getTasks().add(task2);
     String input = "1\n";
     Scanner scanner = new Scanner(input);
@@ -20,4 +23,18 @@ public class TaskManagerTest {
     assertEquals(1, taskManager.getTasks().size());
     assertEquals("Task 2", taskManager.getTasks().get(0).getDescription());
   }
+
+
+  @Test
+  public void testGetUserChoiceValidInput() {
+    String input = "42\n"; // Ввод пользователя
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in); // Перенаправляем System.in для ввода данных
+
+    TaskManager taskManager = new TaskManager();
+    int choice = taskManager.getUserChoice();
+
+    assertEquals(42, choice); // Проверяем, что результат совпадает с ожидаемым значением
+  }
 }
+
