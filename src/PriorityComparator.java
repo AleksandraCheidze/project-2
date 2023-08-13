44586672dbbfb0fdd6cd9d9f0332e84d1b7067e6
Aleksandra Cheidze <a.cxeidze@gmail.com>
2023-08-13 10:22:21 +0200

    feat: add dueDate compare

--- src/PriorityComparator.java
+++ src/PriorityComparator.java
@@ -1,8 +1,16 @@
 import java.util.Comparator;
+import java.util.Date;
 
 public class PriorityComparator implements Comparator<Task> {
   @Override
   public int compare(Task task1, Task task2) {
+    Date dueDate1 = task1.getDueDate();
+    Date dueDate2 = task2.getDueDate();
+
+    int dateComparison = dueDate1.compareTo(dueDate2);
+    if (dateComparison != 0) {
+      return dateComparison;
+    }
     int priority1 = task1.getPriority();
     int priority2 = task2.getPriority();
 
