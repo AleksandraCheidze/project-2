213a371bea58957ee387450277d60b02df710f1e
Aleksandra Cheidze <a.cxeidze@gmail.com>
2023-08-16 15:29:25 +0200

    feat: delete testToStringNoPriority

--- Test/TaskManagerTest.java
+++ Test/TaskManagerTest.java
@@ -145,17 +145,7 @@ public class TaskManagerTest {
     }
   }
 
-  @Test
-  public void testToStringNoPriority() {
-    Task task4 = new Task("No Priority Task");
-    task4.setPriority(0);
-    task4.setCompleted(false);
 
-    String expectedOutput = "No Priority Task [\u001B[32mНе выполнено\u001B[0m] [Приоритет: Нет приоритета] ";
-    String actualOutput = task4.toString();
-
-    assertEquals(expectedOutput, actualOutput);
-  }
 
 }
 
