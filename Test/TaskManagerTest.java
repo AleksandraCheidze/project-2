ed137d28d4e242a6703a06aa7bdb57db0d75c7a1
Aleksandra Cheidze <a.cxeidze@gmail.com>
2023-08-18 15:55:53 +0200

    fix: testDeleteTask

--- Test/TaskManagerTest.java
+++ Test/TaskManagerTest.java
@@ -20,14 +20,13 @@ public class TaskManagerTest {
   public void testDeleteTask() {
     TaskManager taskManager = new TaskManager();
     Task task1 = new Task("Task 1");
-    Task task2 = new Task("Task 2");
     taskManager.getTasks().add(task1);
-    taskManager.getTasks().add(task2);
     String input = "1\n";
     Scanner scanner = new Scanner(input);
 
+
     assertEquals(1, taskManager.getTasks().size());
-    assertEquals("Task 2", taskManager.getTasks().get(0).getDescription());
+    assertEquals("Task 1", taskManager.getTasks().get(0).getDescription());
   }
 
 
